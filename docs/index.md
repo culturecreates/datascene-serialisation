9b088b672b2e002e65d3554e89e0670e7a366806
Gregory Saumier-Finch <gregory@culturecreates.com>
2025-04-10 09:25:19 -0400

    added all main class doc

--- docs/index.md
+++ docs/index.md
@@ -5,6 +5,12 @@ Ceci est généré par json-schema-for-humans.
 
 - [Spectacle](spectacle.html)
 - [Représentation](représentation.html)
+- [Série](série.html)
 - [Offre](offre.html)
+- [Lieu](lieu.html)
+- [Salle](salle.html)
+- [Contributeur](contributeur.html)
+- [Oeuvre](oeuvre.html)
+
 
 
--- generate.sh
+++ generate.sh
@@ -2,3 +2,8 @@
 generate-schema-doc /Users/saumier/Sites/Datascene/schema/spectacle.schema.json ./docs/spectacle.html
 generate-schema-doc /Users/saumier/Sites/Datascene/schema/représentation.schema.json ./docs/représentation.html
 generate-schema-doc /Users/saumier/Sites/Datascene/schema/offre.schema.json ./docs/offre.html
+generate-schema-doc /Users/saumier/Sites/Datascene/schema/contributeur.schema.json ./docs/contributeur.html
+generate-schema-doc /Users/saumier/Sites/Datascene/schema/lieu.schema.json ./docs/lieu.html
+generate-schema-doc /Users/saumier/Sites/Datascene/schema/oeuvre.schema.json ./docs/oeuvre.html
+generate-schema-doc /Users/saumier/Sites/Datascene/schema/salle.schema.json ./docs/salle.html
+generate-schema-doc /Users/saumier/Sites/Datascene/schema/serie.schema.json ./docs/serie.html
